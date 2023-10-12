f60383fc0bef10763ebeed323d912414b7abcabe
lorraineC26 <lorraine.quinyee@gmail.com>
2023-10-11 21:18:41 -0400

    add Ajax GET request for load tweets fx; add timeAgo

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,33 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
 const renderTweets = function(tweetsArr) {
   // loops through tweets
   // calls createTweetElement for each tweet
@@ -42,7 +15,8 @@ const renderTweets = function(tweetsArr) {
 };
 
 const createTweetElement = function(tweetObj) {
-  let layout = `
+  let daysAgo = timeago.format(tweetObj["created_at"]);
+  const layout = `
       <article>
         <header>
           <img class="avatars" src="${tweetObj.user.avatars}"/>
@@ -52,7 +26,7 @@ const createTweetElement = function(tweetObj) {
         <p>${tweetObj.content.text}</p>
 
         <footer>
-          ${tweetObj["created_at"]} days ago
+          <div class="timeago">${daysAgo}</div>
           <div>
             <i class="fa-solid fa-flag" ></i>
             <i class="fa-solid fa-retweet" ></i>
@@ -72,10 +46,16 @@ $(document).ready(function() {
 
     $.ajax({ url: "/tweets", method: 'POST', data: text })
       .then(console.log(text));
-
   });
+  
+  const loadTweets = function() {
+    $.get("/tweets", function(data) {
+      renderTweets(data);
+    });
+  };
 
-  renderTweets(data);
+  loadTweets();
+  
 });
 
 
